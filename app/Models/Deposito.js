938b2c770da79d96b820902946f917ed961fd667
Everton P <evertonportela@outlook.com>
2020-09-30 11:38:10 -0300

    Update - Inclusão de tabelas
    Inclusão de Tabelas, Criação de script de povoamento

--- app/Models/Deposito.js
+++ app/Models/Deposito.js
@@ -3,15 +3,15 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Regiao extends Model {
-    static boot() {
+class Deposito extends Model {
+    static boot () {
         super.boot()
         this.addTrait('NoTimestamp')
     }
 
-    static get primaryKey() {
-        return 'regiao_id'
+     static get primaryKey() {
+        return 'depositos_id'
     }
 }
 
-module.exports = Regiao
+module.exports = Deposito
